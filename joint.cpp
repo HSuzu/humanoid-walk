79a4ecd5d8a493f8fb10b5fcc651cbd0acd18a2b
Henry Suzukawa <henry.shinji@gmail.com>
2019-01-29 08:27:44 -0200

    Change joint function: add phase [0, 2pi] and correct amplitude for the positive and negative semi-cycle

--- joint.cpp
+++ joint.cpp
@@ -19,9 +19,10 @@ void Joint::setJointStats(float posAmp, float negAmp, float neutralAngle, float 
     _negAmp = negAmp;
     _initPhase = phase;
     _neutralAngle = neutralAngle;
-    _tDelta = step_ms/T_ms;
+    _period = T_ms;
+
 #ifdef DEBUG
-    std::cout << "[ " << _name << " ] " <<std::endl;
+    std::cout << "[ " << _name.toStdString() << " ] " <<std::endl;
     std::cout << "PosAmp: " << std::setprecision(2) << _posAmp << ", ";
     std::cout << "NegAmp: " << std::setprecision(2) <<  _negAmp << ", ";
     std::cout << "NeutralAngle: " << std::setprecision(2) << _neutralAngle << ", ";
@@ -31,24 +32,32 @@ void Joint::setJointStats(float posAmp, float negAmp, float neutralAngle, float 
 
 void Joint::update() {
     if (!_enabled) return;
-//    float amplitude = (0 < _t && _t < 0.5) || (-1 < _t && _t < -0.5) ? _posAmp : _negAmp;
-    float amplitude = _t < 0.5f ? _posAmp : _negAmp;
-    float newAngle = _neutralAngle + _amplFactor * amplitude * sin(2 * PI * _t + _initPhase);
-    _t += _tDelta;
+    // Relative position of the moviment if we were in a senoidal function of period 1s
+    float senPos = _t/_period + _initPhase/2*PI;
+    senPos = senPos - (int)senPos;
 
-    if (_t >= 1.0f) {
-        _t = 0;
-        if (_amplFactor < 1.0f) _amplFactor += 0.25f;
+    float amplitude = senPos < 0.5f ? _posAmp : _negAmp;
+    float w = 2 * PI/_period;
+    float newAngle = _neutralAngle + amplitude * std::sin(_velFactor*w* _t + _initPhase);
+    _t += step_ms;
+
+    if (_velFactor < 0.99f) {
+        _velFactor = 1-std::exp(-(_t/_dampingFactor));
+
+        if(_velFactor >= 0.99f) {
+            _velFactor = 1.0f;
+        }
     }
 
-    this->setJointTargetPosition(newAngle);
+    if(newAngle == newAngle) {
+        this->setJointTargetPosition(newAngle);
+    }
 }
 
 void Joint::reset() {
-//    _t = _initPhase;
     _t = 0.0f;
-    _amplFactor = 0.0;
-    this->setJointTargetPosition(_initAngle);
+    _velFactor = 0.0f;
+    this->setJointTargetPosition(_neutralAngle);
 }
 
 void Joint::setJointTargetPosition(float targetAngle) {
--- joint.hpp
+++ joint.hpp
@@ -2,37 +2,41 @@
 #define JOINT_H
 
 #include "VRepClass.hpp"
+#include <cmath>
 
 class JointInterface
 {
 public:
-  JointInterface(){}
-  virtual ~JointInterface(){}
-  virtual void setJointStats(float posAmp, float negAmp, float neutralAngle, float phase, float T) = 0;
-  virtual void update() = 0;
-  virtual void reset() = 0;
-  bool _enabled = true;
+    JointInterface(){}
+    virtual ~JointInterface(){}
+    virtual void setJointStats(float posAmp, float negAmp, float neutralAngle, float phase, float T) = 0;
+    virtual void update() = 0;
+    virtual void reset() = 0;
+    bool _enabled = true;
 };
 
 class Joint : public VRepClass, public JointInterface {
 private:
-  float _posAmp;
-  float _negAmp;
-  float _neutralAngle;
-  float _t;
-  float _tDelta;
-  float _initPhase;
-  float _amplFactor = 0;
-  float _velFactor = 0;
-  simxFloat _initAngle;
+    float _posAmp; // Amplitude for the positive semi-cycle of the senoidal function
+    float _negAmp; // Amplitude for the negative semi-cycle of the senoidal function
+    float _neutralAngle; // The angle that the joint move around
+    float _t; // Current time of simulation
+    float _period; // Period of the movement (ms)
+//    float _tDelta;
+    const float _dampingFactor = 300; // This control the time for the function to reach its peak (time approx DP * 3.5)
+    float _amplFactor = 0;
+//    float _velFactor = 0;
+    float _initPhase;
+
+    simxFloat _initAngle;
 public:
-  void setJointTargetPosition(float targetAngle);
-  Joint(int clientID, const char *jointName);
-  void setJointStats(float posAmp, float negAmp, float neutralAngle, float phase, float T_ms);
-  void update();
-  void reset();
-  float _currentAngle;
-  float _neutralAngleDelta;
+    void setJointTargetPosition(float targetAngle);
+    Joint(int clientID, const char *jointName);
+    void setJointStats(float posAmp, float negAmp, float neutralAngle, float phase, float T_ms);
+    void update();
+    void reset();
+
+    float _currentAngle;
 };
 
 #endif
